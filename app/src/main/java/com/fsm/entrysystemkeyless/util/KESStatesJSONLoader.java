015b29cad73a35c865b833849e57cf4555c8be74
NazDov <nazardovhuy@gmail.com>
2018-02-27 13:18:55 +0200

    add api level resctr

--- app/src/main/java/com/fsm/entrysystemkeyless/util/KESStatesJSONLoader.java
+++ app/src/main/java/com/fsm/entrysystemkeyless/util/KESStatesJSONLoader.java
@@ -1,6 +1,12 @@
 package com.fsm.entrysystemkeyless.util;
 
+<<<<<<< HEAD
 import android.content.Context;
+=======
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+>>>>>>> init commit
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,6 +23,10 @@ public class KESStatesJSONLoader {
 
     private String json;
 
+<<<<<<< HEAD
+=======
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+>>>>>>> init commit
     public KESStatesJSONLoader(Context context, String jsonFileName) {
         try (BufferedReader br = new BufferedReader(
                 new InputStreamReader(openAssets(context, jsonFileName)))) {
